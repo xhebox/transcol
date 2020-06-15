c021ff2db014188077af6fcc19e21fe181805652
xhe <xw897002528@gmail.com>
2020-06-16 01:08:24 +0800

    fix the translation

--- download.sh
+++ download.sh
@@ -2,4 +2,4 @@
 set -ex
 export https_proxy=socks5://127.0.0.1:1080
 export http_proxy=socks5://127.0.0.1:1080
-youtube-dl -i PLUl4u3cNGP63VIBQVWguXxZZi0566y7Wf --playlist-start 7 -f bestvideo[ext=mp4]+bestaudio[ext=m4a] --write-sub
+youtube-dl -i PLUl4u3cNGP63VIBQVWguXxZZi0566y7Wf --playlist-start 3 --playlist-end 3 -f bestvideo[ext=mp4]+bestaudio[ext=m4a] --write-sub
