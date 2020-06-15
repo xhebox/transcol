02ee57045ef9399678ebb029aab88280bedf4c04
xhe <xw897002528@gmail.com>
2020-06-16 00:38:25 +0800

    add P3 translation

--- burn.sh
+++ burn.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 set -ex
 rm -rf ${3}
-ffmpeg -hwaccel vaapi -i "${1}" -vf "subtitles=${2}:force_style='FontName=Source Han Sans SC,OutlineColor=&H292e12&,PrimaryColour=&Hfbffff&'" -vcodec h264 -acodec aac -ar 44100 -pix_fmt yuv420p -ac 2 "${3}"
+ffmpeg -hwaccel vaapi -i "${1}" -vf "subtitles=${2}:force_style='FontName=Source Han Sans SC,OutlineColor=&H292e12&,PrimaryColour=&Hfbffff&'" -vcodec h264 -acodec aac -ar 44100 -pix_fmt yuv420p -ac 2 -s hd1080 "${3}"
---
+++ download.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+set -ex
+export https_proxy=socks5://127.0.0.1:1080
+export http_proxy=socks5://127.0.0.1:1080
+youtube-dl -i PLUl4u3cNGP63VIBQVWguXxZZi0566y7Wf --playlist-start 7 -f bestvideo[ext=mp4]+bestaudio[ext=m4a] --write-sub
